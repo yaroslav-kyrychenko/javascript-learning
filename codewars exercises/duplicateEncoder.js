00cb45bff84c1c0cf8dc6aa4ac7928f807c16ef8
Yaroslav Kyrychenko <62503200+yaroslav-kyrychenko@users.noreply.github.com>
2022-02-20 14:26:09 +0100

    Update duplicateEncoder.js

--- codewars exercises/duplicateEncoder.js
+++ codewars exercises/duplicateEncoder.js
@@ -1,17 +1,8 @@
-// Nie działa - zamiast arrWord, które później nadpisujemy lepiej stwórzyć pustą tablicę i dodawać do niej ( albo ) w zależności od wyniku warunku.
-// const duplicateEncode = function (word) {
-//   let arrWord = word.toLowerCase().split('');
-//   for (let i = 0; i < arrWord.length; i++) {
-//     if (arrWord.indexOf(arrWord[i]) !== arrWord.lastIndexOf(arrWord[i])) {
-//       arrWord[i] = ')';
-//     } else {
-//       arrWord[i] = '(';
-//     }
-//   }
-//   return arrWord.join('');
-// };
+// The goal of the exercise is to create an encoder for strings. 
+// Where a symbol is repeated in a string, it should be encoded as ')'.
+// Where it is unique, it should be encoded as '('.
 
-// Sposób myślenia jak powyżej, ale poprawnie działające
+// Method 1 - using lastIndexOf.
 const duplicateEncode = function (word) {
   let encodedArr = [];
   word = word.toLowerCase();
@@ -25,20 +16,21 @@ const duplicateEncode = function (word) {
   return encodedArr.join("");
 };
 
-// const duplicateEncode = function (word) {
-//   word = word.toLowerCase();
-//   let newText = '';
-//   for (let i in word)
-//     if (word.split(word[i]).length - 1 > 1) {
-//       newText += ')';
-//     } else {
-//       newText += '(';
-//     }
+// Method 2 - using split.
+const duplicateEncode = function (word) {
+  word = word.toLowerCase();
+  let newText = '';
+  for (let i in word)
+    if (word.split(word[i]).length - 1 > 1) {
+      newText += ')';
+    } else {
+      newText += '(';
+    }
+  return newText;
+};
 
-//   return newText;
-// };
-
-console.log(duplicateEncode("din"));
-console.log(duplicateEncode("recede"));
-console.log(duplicateEncode("Success"));
-console.log(duplicateEncode("(( @"));
+// Tests
+console.log(duplicateEncode("din")); // results in (((
+console.log(duplicateEncode("recede")); // results in ()()() 
+console.log(duplicateEncode("Success")); // results in )())())
+console.log(duplicateEncode("(( @")); // results in ))((
