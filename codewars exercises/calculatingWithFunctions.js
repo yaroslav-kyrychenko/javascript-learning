175a1dac08bd64114adcca5b78989f8a075577b2
Yaroslav Kyrychenko <62503200+yaroslav-kyrychenko@users.noreply.github.com>
2022-11-19 19:30:35 +0100

    Solved exercise, refactoring may be needed

--- codewars exercises/calculatingWithFunctions.js
+++ codewars exercises/calculatingWithFunctions.js
@@ -8,29 +8,94 @@
 
 // These functions should accept a callback function in case of left operand and should simply return a value in case of right operand.
 
-function zero(mathematicalOperation) {
-  console.log(typeof mathematicalOperation);
-  if (mathematicalOperation && typeof mathematicalOperation == 'function')
-    mathematicalOperation();
+let rightOperand;
+let leftOperand;
+let result;
+
+function assignOperandsAndDoMath(mathOperation, digit) {
+  if (!mathOperation) rightOperand = digit;
+  if (typeof mathOperation == 'function') {
+    leftOperand = digit;
+    mathOperation(leftOperand, rightOperand);
+  }
 }
-function one() {}
-function two() {}
-function three() {}
-function four() {}
-function five() {}
-function six() {}
-function seven() {}
-function eight() {}
-function nine() {}
+
+function zero(mathOperation) {
+  digit = 0;
+  assignOperandsAndDoMath(mathOperation, digit);
+  return result;
+}
+function one(mathOperation) {
+  digit = 1;
+  assignOperandsAndDoMath(mathOperation, digit);
+  return result;
+}
+function two(mathOperation) {
+  digit = 2;
+  assignOperandsAndDoMath(mathOperation, digit);
+  return result;
+}
+function three(mathOperation) {
+  digit = 3;
+  assignOperandsAndDoMath(mathOperation, digit);
+  return result;
+}
+function four(mathOperation) {
+  digit = 4;
+  assignOperandsAndDoMath(mathOperation, digit);
+  return result;
+}
+function five(mathOperation) {
+  digit = 5;
+  assignOperandsAndDoMath(mathOperation, digit);
+  return result;
+}
+function six(mathOperation) {
+  digit = 6;
+  assignOperandsAndDoMath(mathOperation, digit);
+  return result;
+}
+function seven(mathOperation) {
+  digit = 7;
+  assignOperandsAndDoMath(mathOperation, digit);
+  return result;
+}
+function eight(mathOperation) {
+  digit = 8;
+  assignOperandsAndDoMath(mathOperation, digit);
+  return result;
+}
+function nine(mathOperation) {
+  digit = 9;
+  assignOperandsAndDoMath(mathOperation, digit);
+  return result;
+}
 
 // These function should somehow read the left operand and calculate using the right operand
 
 function plus() {
-  console.log(0);
+  return function (leftOperand, rightOperand) {
+    result = leftOperand + rightOperand;
+  };
 }
-function minus() {}
-function times() {}
-function dividedBy() {}
+function minus() {
+  return function (leftOperand, rightOperand) {
+    result = leftOperand - rightOperand;
+  };
+}
+function times() {
+  return function (leftOperand, rightOperand) {
+    result = leftOperand * rightOperand;
+  };
+}
+function dividedBy() {
+  return function (leftOperand, rightOperand) {
+    result = Math.trunc(leftOperand / rightOperand);
+  };
+}
 
-// one(plus(two())); // must output 3
-zero(plus());
+console.log(two(plus(one())));
+console.log(five(minus(three())));
+console.log(two(times(eight())));
+console.log(seven(dividedBy(nine())));
+console.log(four(dividedBy(zero())));
